8a524719b7c8f23bc69a4379666ba07c236b7e95
subhandp <subhan.dinda.putra@gmail.com>
2023-03-21 22:31:11 +0800

    add new url cicd

--- server.test.js
+++ server.test.js
@@ -2,10 +2,10 @@ const app = require('./server') // Link to your server file
 const supertest = require('supertest')
 const request = supertest(app)
 
-it('Call the /devops endpoint', async ()=> {
-    const res = await request.get('/devops')
+it('Call the /cicd endpoint', async ()=> {
+    const res = await request.get('/cicd')
     expect(res.status).toBe(200)
-    expect(res.text).toBe('devops start journey at 19/03/2023')
+    expect(res.text).toBe('cicd')
 })
 
 it('Call the /baru endpoint', async ()=> {
@@ -13,6 +13,13 @@ it('Call the /baru endpoint', async ()=> {
     expect(res.status).toBe(200)
     expect(res.text).toBe('api baru')
 })
+
+it('Call the /devops endpoint', async ()=> {
+    const res = await request.get('/devops')
+    expect(res.status).toBe(200)
+    expect(res.text).toBe('devops start journey at 19/03/2023')
+})
+
 
 it('Call the / endpoint', async () => {
     const res = await request.get('/')
